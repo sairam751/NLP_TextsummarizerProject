88dac2161d95080f0b21b95d94c39ffd4e86595b
sairam751 <sairam751348@gmail.com>
2025-03-11 12:27:15 +0530

    Data_ingestion step

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.NLP_textSummarizer.logging import logger
+from src.NLP_textSummarizer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f"Initiating {STAGE_NAME}")
+    data_ingestion_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Completed {STAGE_NAME}")
+except Exception as e:
+    logger.error(f"Failed to execute {STAGE_NAME}")
+    logger.error(str(e))
+    raise e
+
 
 logger.info("Hello World")
