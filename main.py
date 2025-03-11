e1b2e7dad8aff00565ab1ab9c5aae0c07afcc322
sairam751 <sairam751348@gmail.com>
2025-03-11 13:30:43 +0530

    `Added Model Trainer stage and configuration files`

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.NLP_textSummarizer.pipeline.model_trainer import ModelTrainerTrainingPipeline
 from src.NLP_textSummarizer.logging import logger
 from src.NLP_textSummarizer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.NLP_textSummarizer.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
@@ -20,6 +21,18 @@ try:
     logger.info(f"stage {STAGE_NAME} initiated")
     data_ingestion_pipeline=DataTransformationTrainingPipeline()
     data_ingestion_pipeline.initiate_data_transformation()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME="Model Trainer stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    model_trainer_pipeline=ModelTrainerTrainingPipeline()
+    model_trainer_pipeline.initiate_model_trainer()
     logger.info(f"Stage {STAGE_NAME} Completed")
 except Exception as e:
     logger.exception(e)
---
+++ src/NLP_textSummarizer/pipeline/model_trainer.py
@@ -0,0 +1,12 @@
+from src.NLP_textSummarizer.config.configuration import ConfigurationManager
+from src.NLP_textSummarizer.components.model_trainer import ModelTrainer
+from src.NLP_textSummarizer.logging import logger
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+    def initiate_model_trainer(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
